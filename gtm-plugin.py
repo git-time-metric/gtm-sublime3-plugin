363eab69d2587ca735fb9617d9db77154906fba0
Michael Schenk <mschenk@edgeg.io>
2016-05-29 18:08:15 -0500

    Search for gtm executable on path

--- gtm-plugin.py
+++ gtm-plugin.py
@@ -1,16 +1,46 @@
 import shlex
 import sublime
 import sublime_plugin
+import sys
 import time
 import os
+
+def find_gtm(gtm):
+    # Adapted from https://gist.github.com/4368898
+    # Public domain code by anatoly techtonik <techtonik@gmail.com>
+    # AKA Linux `which` and Windows `where`
+
+    path = os.environ['PATH']
+    extlist = ['']
+    if sys.platform == 'win32':
+        pathext = os.environ['PATHEXT'].lower().split(os.pathsep)
+        (base, ext) = os.path.splitext(gtm)
+        if ext.lower() not in pathext:
+            extlist = pathext
+    else:
+        # add in the typical brew executable path
+        path = path + ':/usr/local/bin:/usr/local/sbin'
+
+    for ext in extlist:
+        execname = gtm + ext
+        if os.path.isfile(execname):
+            return execname
+        else:
+            paths = path.split(os.pathsep)
+            for p in paths:
+                f = os.path.join(p, execname)
+                if os.path.isfile(f):
+                    return f
+
+    return None
 
 class GTM(sublime_plugin.EventListener):
     update_interval = 30
     last_update = 0.0
     last_path = None
 
-    # TODO: prompt for location of GTM or use a configuration file?
-    gtm_path = "/Users/mschenk/Projects/go/bin"
+    # TODO: how to handle if gtm not found?
+    gtm_path = find_gtm('gtm')
 
     def on_post_save_async(self, view):
         self.record(view, view.file_name())
@@ -33,5 +63,5 @@ class GTM(sublime_plugin.EventListener):
             GTM.last_update = time.time()
             GTM.last_path = path
 
-            cmd = "{0}/gtm record {1}".format(GTM.gtm_path, path)
+            cmd = "{0} record {1}".format(GTM.gtm_path, path)
             view.window().run_command('exec', {'cmd': shlex.split(cmd)})
